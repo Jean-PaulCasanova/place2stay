cc1cbe75a3f201e965c8075b55f321f890e3caee
jean-paulcasanova <japes365@gmail.com>
2025-03-05 18:34:47 -0500

    phase one complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/hello/world', (req, res) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+// Import the API router
+const apiRouter = require('./api');
+
+
+
+router.get("/api/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
 });
+
+//router.get('/hello/world', (req, res) => {
+//  res.cookie('XSRF-TOKEN', req.csrfToken());
+//  res.send('Hello World!');
+//});
+
+// Connect it to `/api`
+router.use('/api', apiRouter);
+
 
 module.exports = router;
