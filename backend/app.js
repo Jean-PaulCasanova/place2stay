9513bb3b79470c485976c7bf65077b9e34e087ba
jean-paulcasanova <japes365@gmail.com>
2025-03-06 20:53:46 -0500

    phase two half done

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -29,5 +30,21 @@ app.use(
 
 const routes = require('./routes');
 app.use(routes);
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    //check if error is a sequelize error:
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+})
 
 module.exports = app;
