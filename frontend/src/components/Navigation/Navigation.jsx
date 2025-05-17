762bfd795a6fa192df1ccd74ffd8a73f73f307be
jean-paulcasanova <japes365@gmail.com>
2025-05-17 17:55:23 -0400

    profile drop down tinkering

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,22 +1,16 @@
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css'
-import * as sessionActions from '../../store/session';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
   
 
   const sessionLinks = sessionUser ? (
-    <>
       <li>
         <ProfileButton user={sessionUser} />
-      </li>
-      <li>
-        <button onClick={logout}>Log Out</button>
-      </li>
-    </>
+      </li>  
   ) : (
     <>
       <li>
